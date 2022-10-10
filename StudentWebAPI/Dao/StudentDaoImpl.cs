b4fd47e1490e261d403c48bbdd48971ae7badb6a
wangwang.zhang <wangwang.zhang@thoughtworks.com>
2022-10-10 09:40:43 +0800

    Added some new data items in StudentDaoImpl

--- StudentWebAPI/Dao/StudentDaoImpl.cs
+++ StudentWebAPI/Dao/StudentDaoImpl.cs
@@ -8,6 +8,10 @@ public class StudentDaoImpl : IStudentDao
     {
         new Student("1", "Tom", "13567845677",10),
         new Student("2", "Amy", "13867045471",28),
+        new Student("3", "Cindy", "13854716704",12),
+        new Student("4", "David", "13812345678",18),
+        new Student("5", "Lucas", "13587653211",15),
+        new Student("6", "Henry", "13876666666",16),
     };
     public List<Student> GetAll()
     {
