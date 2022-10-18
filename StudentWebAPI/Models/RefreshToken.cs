a618100e98473a792bc3324bedc701db24830f7d
wangwang.zhang <wangwang.zhang@thoughtworks.com>
2022-10-18 16:28:33 +0800

    Implemented class RefreshToken

--- StudentWebAPI/Models/RefreshToken.cs
+++ StudentWebAPI/Models/RefreshToken.cs
@@ -2,5 +2,7 @@ namespace StudentWebAPI.Models;
 
 public class RefreshToken
 {
-    
+    public string Token { get; set; } = string.Empty;
+    public DateTime Created { get; set; } = DateTime.Now;
+    public DateTime Expires { get; set; }
 }
